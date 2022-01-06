97316f6712edbadcc4437374d00d05d41edcf70f
Jack Rose <user@jackrose.co.nz>
2022-01-06 21:45:41 +1100

    efs episode 8 lsp set up

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
 ;; stop C-<direction> keys doing weird stuff
 (global-set-key (kbd "C-j") nil)
 (global-set-key (kbd "C-k") nil)
-(global-set-key (kbd "C-l") nil)
+;; (global-set-key (kbd "C-l") nil)
 
 
 ;; Initialize package sources
@@ -141,11 +141,11 @@
 (rune/leader-keys
     "fb" '(counsel-ibuffer :which-key "switch buffers")
     "fp" '(projectile-switch-project :which-key "switch project")
-    "ff" '(counsel-find-file :which-key "find files"))
+    "ff" '(projectile--find-file :which-key "find files"))
 (rune/quick-keys
     "b" '(counsel-ibuffer :which-key "switch buffers")
     "p" '(projectile-switch-project :which-key "switch project")
-    "f" '(counsel-find-file :which-key "find files"))
+    "f" '(projectile--find-file :which-key "find files"))
 
 (use-package undo-tree
   :init
@@ -304,7 +304,72 @@
   :hook (org-mode . efs/org-mode-visual-fill))
 
 ;; LSP Mode Configuration ------------------------------------------------------
-;; TODO
+(defun self/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . self/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-l")  ;; TODO set this up so it's =SPC l=
+  :config
+  (lsp-enable-which-key-integration t)
+  :custom
+  ;; TODO why isn't this working?? https://github.com/emacs-lsp/lsp-mode/issues/1223#issuecomment-586674535
+  (lsp-signature-auto-activate nil))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (define-key evil-normal-state-map (kbd "gh") 'lsp-ui-doc-show)
+  :custom
+  (lsp-ui-doc-position 'top)) ;; can be top, bottom, or at-point
+
+;; lsp-ivy-workspace-symbol - Search for a symbol name in the current project workspace
+;; lsp-ivy-global-workspace-symbol - Search for a symbol name in all active project workspaces
+(use-package lsp-ivy)
+
+
+;; Company mode configuration ------------------------------------------------------
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+;; lsp leader keys
+;; (rune/leader-keys
+;;     "la" '(projectile--find-file :which-key "code action"))
+
+;; Langauge configuration ------------------------------------------------------
+(use-package typescript-mode
+  :mode "\\.ts\\'\\|\\.tsx\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
+(use-package tree-sitter
+  :ensure t
+  :hook ((typescript-mode . tree-sitter-hl-mode)
+	 (typescript-tsx-mode . tree-sitter-hl-mode)))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter
+  :config
+  (tree-sitter-require 'tsx)
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-mode . tsx)))
+
+;; TODO tsx
 
 
 ;; My misc other configuration ------------------------------------------------------
@@ -329,7 +394,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(no-littering evil-surround undo-tree pdf-tools evil-org gnuplot evil-numbers doom-themes forge evil-magit magit counsel-projectile projectile use-package)))
+   '(company-box company tree-sitter-langs tree-sitter typescript-mode lsp-ivy evil-surround no-littering lsp-ui lsp-mode undo-tree pdf-tools evil-org gnuplot evil-numbers doom-themes forge evil-magit magit counsel-projectile projectile use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
